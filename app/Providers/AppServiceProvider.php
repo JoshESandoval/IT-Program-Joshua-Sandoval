50332ce09ab304fe3e672ffaa04e69254432987e
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-16 18:07:41 -0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+    public function boot(UrlGenerator $url)
+    {
+        if(env('REDIRECT_HTTPS')){
+            $url->formatScheme('https');
+        }
+    }
+    
     public function register()
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+        if(env('REDIRECT_HTTPS')){
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
