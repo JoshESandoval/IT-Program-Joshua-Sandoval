dafd2623fe41d6f537b20d8cbf8ec79389df2a04
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-27 19:55:09 -0700

    Update createEquiptment.blade.php

--- resources/views/createEquiptment.blade.php
+++ resources/views/createEquiptment.blade.php
@@ -3,5 +3,10 @@
  @section('title', 'Create Equiptment')
 
  @section('content_header')
+    <h1> Create Equiptment </h1>
+@stop
 
- 
+@section('content')
+    <p>Edit Information to create Equiptment</p>
+@stop 
+
