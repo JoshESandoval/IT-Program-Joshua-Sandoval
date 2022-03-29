df5e930445c0c4bf58ebc0c60b0cac53b8f74335
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-28 22:15:34 -0700

    Update createEquiptment.blade.php

--- resources/views/createEquiptment.blade.php
+++ resources/views/createEquiptment.blade.php
@@ -8,17 +8,21 @@
 
 @section('content')
     <p>Edit Information to create Equiptment</p>
-    <form class="form-inline" method="post" action="{{}}">
-        @csrf 
-        <input type="input" class=" "name="Type" placeholder="tablet"/>
-       
-        <div>Manufacturer</div>
-        <select class="btn btn-info" name="Type" id="TypeSelect">
-            <option value="Lenovo">Lenovo</option>
-            <option value="Apple">Apple</option>
-            <option value="Dell">Dell</option>
-            <option value="Microsoft">Microsoft</option>
-        </select>
+    <form  method="post" action="{{}}">
+        @csrf
+        <div>
+            Type
+            <input type="input" class=" "name="Type" placeholder="tablet"/>
+        </div>
+
+        <div>Manufacturer
+            <select class="btn btn-info" name="Type" id="TypeSelect">
+                <option value="Lenovo">Lenovo</option>
+                <option value="Apple">Apple</option>
+                <option value="Dell">Dell</option>
+                <option value="Microsoft">Microsoft</option>
+            </select>
+        </div>
 
         <x-adminlte-button type="submit" label="Submit"/>
     </form>
@@ -27,4 +31,6 @@
 @section('js')
 	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    
+
 @stop
