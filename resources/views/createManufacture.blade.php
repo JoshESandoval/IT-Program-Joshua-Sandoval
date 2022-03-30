a774d55986b9db3a376fb48659322de28b6eb18d
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-29 21:33:48 -0700

    Update createManufacture.blade.php

--- resources/views/createManufacture.blade.php
+++ resources/views/createManufacture.blade.php
@@ -30,15 +30,19 @@
 
         <div class="form-group">
             Email
+            <div class="col-sm-5">
             <input type="email" name="TechEmail" placeholder="BlackSmith@example.net"> 
+            </div>
         </div>
         <div class="form-group">
             Phone #
+
             <input type="tel" name="SaleNumber" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="909-867-5309">
         </div>
 
         <input type="submit" label="Submit">
-
+        <br>
+        <br>
 @stop
 
 
