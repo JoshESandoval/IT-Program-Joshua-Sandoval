cd5cd81cbd3fc40d2ce36bc5451c14ad07c813f8
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-30 02:51:10 -0700

    Update create.blade.php

--- resources/views/manufactures/create.blade.php
+++ resources/views/manufactures/create.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-    <form method="POST" name="manufactureI" class="horizoltal-group" action="{{ route('manufactuers.store')}}">
+    <form method="POST" name="manufactureI" class="horizoltal-group" action="{{ route('manufacturs.store')}}">
         @csrf
         <div class="form-group">
             Manufacture
