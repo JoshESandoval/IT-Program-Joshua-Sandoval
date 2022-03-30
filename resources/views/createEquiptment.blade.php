68711bf3f7c728b3644963485b924c952440a8ec
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-29 18:40:10 -0700

    Update createEquiptment.blade.php

--- resources/views/createEquiptment.blade.php
+++ resources/views/createEquiptment.blade.php
@@ -90,7 +90,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     <script> 
     function display(){
-        alert(document.form1);
+        alert(document.form1.Type);
     }
     </script>
 @stop
