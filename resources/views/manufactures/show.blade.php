1386c6699a390c594f34f1d73e0dac4b07142d30
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-30 05:12:01 -0700

    af

---
+++ resources/views/manufactures/show.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+
+@section('title','View Manufacture')
+
+@section('content_header')
+    <h1>Manufacture Details</h1>    
+@endsection
+
+@section('content')
+    <div>Name:{{$manufacture->title;}}</div>
+    <div>Sales Email:{{$manufacture->salesEmail;}}</div>
+    <div>Sales Phone Number:{{$manufacture->salesNumber;}}</div>
+    <div>Tech Support Email:{{$manufacture->techEmail;}}</div>
+    <div>Tech Support Number:{{$manufacture->techNumber;}}</div>
+@stop
