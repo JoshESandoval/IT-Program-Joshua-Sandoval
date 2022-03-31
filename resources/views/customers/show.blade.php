b36c9a30ee52286f9578c167858efb9c503d16de
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-03-30 17:29:19 -0700

    Update show.blade.php

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     <div>ID: {{$customer->id;}}</div>
     <div>First Name: {{$customer->fName;}}</div>
-    <div>Last Name: {{$customer->slName;}}</div>
+    <div>Last Name: {{$customer->lName;}}</div>
     <div>Email: {{$customer->cEmail;}}</div>
     <div>Phone : {{$customer->cNumber;}}</div>
     <a class='btn btn-default' href="{{route('customers.edit', ['customer'=>$customer->id]) }}">Edit</a>
